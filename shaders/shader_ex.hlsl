514bf4a5ff286bc997da9e63a3ec4e80619f7b28
cooperg456 <166761371+cooperg456@users.noreply.github.com>
2025-12-02 22:33:15 -0500

    cmake stuff

---
+++ shaders/shader_ex.hlsl
@@ -0,0 +1,4 @@
+//	placeholder to test build system
+
+[numthreads(1, 1, 1)]
+void main(uint3 DTid : SV_DispatchThreadID) {}
---
+++ src/cpp_ex.cpp
@@ -0,0 +1,6 @@
+#include <vulkan/vulkan.hpp>
+#include <vma/vk_mem_alloc.h>
+
+int main() {
+	return 0;
+}
